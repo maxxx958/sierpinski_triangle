48f751e0472e3936cc810ad8d4407c250b53ee3f
Max <m4ybe958@gmail.com>
2024-06-21 11:28:50 +0200

    speed adjustment

--- sketch.js
+++ sketch.js
@@ -1,5 +1,3 @@
-// import Point from "./point.js";
-
 function setup() {
     createCanvas(600, 600);
     translate(height / 2, width / 2);
@@ -13,7 +11,7 @@ let points = [new Point(250, 250), new Point(-250, 250), new Point(0, -250)];
 function draw() {
     translate(height / 2, width / 2);
     if (keyIsPressed && keyCode === 32) {
-        for (let i = 0; i < 1000; i++) {
+        for (let i = 0; i < 50; i++) {
             points.push(
                 points[points.length - 1].avg(points[floor(random() * 3)])
             );
