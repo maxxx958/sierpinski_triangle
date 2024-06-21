b14b3c8511b4eff3fd9f03a17d309c735bf97a87
Max <maxymilian.kowalski@codility.com>
2024-06-21 10:08:22 +0200

    init

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(600, 600);
+    background(255);
+}
+
+function draw() {
+    background(255);
+    translate(400, 400);
+    fill(0);
+
+    ellipse(0, 0, 5, 5);
+}
