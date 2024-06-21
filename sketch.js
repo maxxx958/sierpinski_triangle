d398e31faa807c994164fce0f8bab614e68c4d09
Max <m4ybe958@gmail.com>
2024-06-21 11:31:09 +0200

    added text

--- sketch.js
+++ sketch.js
@@ -2,7 +2,9 @@ function setup() {
     createCanvas(600, 600);
     translate(height / 2, width / 2);
     background(0);
-
+    fill(255);
+    textSize(16);
+    text("Hold spacebar to draw", -height / 2 + 20, -width / 2 + 30);
     for (let i = 0; i < 3; i++) {
         points[i].show();
     }
